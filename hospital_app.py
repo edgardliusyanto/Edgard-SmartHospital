0ffcc378caddebd4e8ac26a852eb379f60b4c1a8
edgardliusyanto <edgard.liusyanto@tzuchi.sch.id>
2026-06-25 14:20:28 +0700

    E

--- hospital_app.py
+++ hospital_app.py
@@ -186,10 +186,10 @@ with st.form("triage_form"):
     # Section 4 — Medical History
     st.markdown("""
     <!-- COLOR5 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#4409f6;border:1px solid #710ddc;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#0a2df2;color:rgb(255, 255, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">4</span>
             <span style="font-size:16px;font-weight:600;color:#105efa;">do you have any of the following</span>
         </div>
