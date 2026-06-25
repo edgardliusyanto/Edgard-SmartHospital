88a59387db2e6f9a0187328d16856165aea47ae1
edgardliusyanto <edgard.liusyanto@tzuchi.sch.id>
2026-06-25 15:02:37 +0700

    w

--- hospital_app.py
+++ hospital_app.py
@@ -55,7 +55,7 @@ cc_map        = bundle['cc_map']
 DEPT_INFO = {
     'Respiratory Medicine': {
         'icon':'🫁','color':'#0284c7','bg':'#e0f2fe','border':'#7dd3fc',
-        'desc':'Respiratory medisen',
+        'desc':'specialises in condition affecting the lungs and airways',
         'next':['Visit Level 2, Wing B','Estimated wait: 15–25 min','Please wear a mask']
     },
     'Cardiology': {
