8eb422794b4638df6f76a746932295eb9a211aff
edgardliusyanto <edgard.liusyanto@tzuchi.sch.id>
2026-06-25 14:22:02 +0700

    3E

--- hospital_app.py
+++ hospital_app.py
@@ -144,10 +144,10 @@ with st.form("triage_form"):
     # Section 2 — Duration & Complaint
     st.markdown("""
     <!-- COLOR3 -->
-    <div style="background:#008df1;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#008df1;border:1px solid #0d2fdc;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#630af2;color:rgb(255, 255, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
             <span style="font-size:16px;font-weight:600;color:#105efa;">how long have you have this synthem</span>
         </div>
