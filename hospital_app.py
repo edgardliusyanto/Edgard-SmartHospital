30447c2f4c344c046cf93334c8087bfb4cce3256
edgardliusyanto <edgard.liusyanto@tzuchi.sch.id>
2026-06-25 14:19:38 +0700

    e

--- hospital_app.py
+++ hospital_app.py
@@ -165,10 +165,10 @@ with st.form("triage_form"):
     # Section 3 — Severity
     st.markdown("""
     <!-- COLOR4 -->
-    <div style="background:#0022fe;border:1px solid #ff8f07;border-radius:14px;
+    <div style="background:#0022fe;border:1px solid #075aff;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#22f706;color:rgb(17, 164, 170);border-radius:8px;
+            <span style="background:#6206f7;color:rgb(85, 4, 236);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">3</span>
             <span style="font-size:16px;font-weight:600;color:#105efa;">how would you rate the severity</span>
         </div>
